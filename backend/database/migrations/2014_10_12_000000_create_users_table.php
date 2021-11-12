1e725b6b23f74ef16a00de3bf3f147836707b2b3
tachibana687 <wsamkprimeape@gmail.com>
2021-11-12 10:14:35 +0000

    MigrationFileを作成

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->int('student_number');
+            $table->string('user_name');
+            $table->string('email')->nullable();
+            $table->string('link')->nullable();
+            $table->string('self_introduction')->nullable();
+            $table->timestamps('create_at');
+            $table->primary('student_number');
         });
     }
 
