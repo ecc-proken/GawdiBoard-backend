dbc56be103e15228788d08f61607065317072d10
ATG <40588884+akisu1016@users.noreply.github.com>
2021-11-15 12:24:20 +0900

    Revert "Fix/migration"

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('student_number')->primary();;
-            $table->string('user_name');
-            $table->string('email')->nullable();
-            $table->string('link')->nullable();
-            $table->string('self_introduction')->nullable();
-            $table->timestamps('create_at');
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
