68f988a02e5cd8e2ca54aaa748636db2bb07be99
zulixi <30855961+zulixi@users.noreply.github.com>
2021-11-19 15:45:09 +0900

    Update Tag.php

--- backend/app/Models/Tag.php
+++ backend/app/Models/Tag.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
-    protected $guarded = ['tag_genre_id'];
+    protected $guarded = [''];
     protected $fillable = ['tag_name', 'tag_genre_id'];
     public $timestamps = false;
 }
