9d37280d81a73fdf99e14363cec23d85822ac155
Akisu <akisu1016@yahoo.co.jp>
2021-11-29 14:57:07 +0900

    batchファイル作成

---
+++ backend/app/Console/Commands/OfferDelete.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Offer;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
+class OfferDelete extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'offer:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '掲載期間を過ぎた募集を削除する';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $message = '';
+        // トランザクションの開始
+        DB::transaction(function () use ($message) {
+            $message .= '[' . date('Y-m-d h:i:s') . ']destroied offers:' .
+                Offer::whereDate('end_date', '2021-12-31')->delete();
+        });
+
+        //INFOレベルでメッセージを出力
+        $this->info($message);
+        //ログを書き出す
+        Log::setDefaultDriver('batch');
+        Log::info($message);
+    }
+}
