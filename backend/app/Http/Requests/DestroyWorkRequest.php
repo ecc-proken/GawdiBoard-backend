9f80a6a4567eaabbb6fcabb130cae4d1ccb460de
zulixi <kei.adc11.29@gmail.com>
2021-12-01 02:46:37 +0000

    [fix] DestroyWorkRequestのrule/authorizeを書き忘れていたので追記。

--- backend/app/Http/Requests/DestroyWorkRequest.php
+++ backend/app/Http/Requests/DestroyWorkRequest.php
@@ -13,7 +13,7 @@ class DestroyWorkRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class DestroyWorkRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'work_id' => ['required', 'integer'],
         ];
     }
 }
