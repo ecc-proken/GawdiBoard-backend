a9ee0f05d8450bcfb0ac05dd5e30b7647ad1a67f
Akisu <akisu1016@yahoo.co.jp>
2021-12-03 15:22:22 +0900

    応募送信のバリデーション作成

---
+++ backend/app/Http/Requests/PostOfferApplyRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostOfferApplyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'student_number' => ['required', 'integer', 'digits:7'], #数値かつ7桁
+            'offer_student_number' => ['required', 'integer', 'digits:7'], #数値かつ7桁
+            'interest' => ['required', 'integer'],
+            'user_class' => ['required', 'string'],
+            'message' => ['required', 'string'],
+        ];
+    }
+}
