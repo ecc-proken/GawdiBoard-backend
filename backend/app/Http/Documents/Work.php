f4ac9ca21522c430ef31c61a257513170b68f2ea
murata0531 <coless115@gmail.com>
2022-01-17 09:39:49 +0900

    [add]作品関連apiのドキュメントにサンプルリクエストを追加

--- backend/app/Http/Documents/Work.php
+++ backend/app/Http/Documents/Work.php
@@ -6,6 +6,7 @@
  *   name="work_id",
  *   in="query",
  *   required=true,
+ *   example="1",
  *   description="作品のid",
  * ),
  */
@@ -17,7 +18,7 @@
  *   in="query",
  *   @OA\Schema(
  *        type="array",
- *        @OA\Items(type="integer")
+ *        @OA\Items(type="number")
  *   ),
  *   description="作品タグのidの配列",
  * ),
@@ -41,31 +42,37 @@
  *       @OA\Property(
  *           property="title",
  *           type="string",
+ *           example="百葉箱",
  *           description="作品のタイトル"
  *       ),
  *       @OA\Property(
  *           property="short_description",
  *           type="string",
+ *           example="百葉箱",
  *           description="作品の一言"
  *       ),
  *       @OA\Property(
  *           property="note",
  *           type="string",
+ *           example="詳細~~~~~",
  *           description="作品の備考"
  *       ),
  *       @OA\Property(
  *           property="picture",
  *           type="string",
+ *           example="https://example.com",
  *           description="作品の画像"
  *       ),
  *       @OA\Property(
  *           property="link",
  *           type="string",
+ *           example="https://example.com",
  *           description="作品の参考サイト"
  *       ),
  *       @OA\Property(
  *           property="work_tag_ids",
  *           type="array",
+ * 　　　　　　example="[1,4,6]",
  *           description="作品タグのidの配列",
  *           @OA\Items(
  *               type="number",
@@ -84,36 +91,43 @@
  *       @OA\Property(
  *           property="work_id",
  *           type="number",
+ *           example="1",
  *           description="作品のid"
  *       ),
  *       @OA\Property(
  *           property="title",
  *           type="string",
+ *           example="百葉箱",
  *           description="作品のタイトル"
  *       ),
  *       @OA\Property(
  *           property="short_description",
  *           type="string",
+ *           example="百葉箱",
  *           description="作品の一言"
  *       ),
  *       @OA\Property(
  *           property="note",
  *           type="string",
+ *           example="詳細~~~~~",
  *           description="作品の備考"
  *       ),
  *       @OA\Property(
  *           property="picture",
  *           type="string",
+ *           example="https://example.com",
  *           description="作品の画像"
  *       ),
  *       @OA\Property(
  *           property="link",
  *           type="string",
+ *           example="https://example.com",
  *           description="作品の参考サイト"
  *       ),
  *       @OA\Property(
  *           property="work_tag_ids",
  *           type="array",
+ *           example="[1,4,6]",
  *           description="作品タグのidの配列",
  *           @OA\Items(
  *               type="number",
@@ -132,6 +146,7 @@
  *       @OA\Property(
  *           property="work_id",
  *           type="number",
+ *           example="2",
  *           description="作品のid"
  *       ),
  *   ),
