4d6677b09e0ece6d5b906beaa02889e04f6b9459
yumekiti <yumekiti120440@gmail.com>
2022-01-26 02:47:32 +0900

    add: model UserOffer

---
+++ backend/database/migrations/2022_01_25_174716_create_user_offers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_offers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_offers');
+    }
+}
