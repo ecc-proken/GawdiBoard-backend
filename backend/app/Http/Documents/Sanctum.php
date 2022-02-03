df1c6dace1665be8ac83f1ad666aa14180948706
murata0531 <coless115@gmail.com>
2022-02-04 02:42:02 +0900

    sanctumからクッキーを取得するドキュメントを記述

---
+++ backend/app/Http/Documents/Sanctum.php
@@ -0,0 +1,38 @@
+<?php
+
+# sanctumから認証クッキーを取得するドキュメント
+/**
+ * @OA\Get(
+ *  path="/sanctum/csrf-cookie",
+ *  summary="認証クッキーを取得する(ログインする前に実行して)",
+ *  description="Laravel Sanctumにアクセスして認証クッキーを取得する。chromeの検証ツールからレスポンスヘッダの「X-XSRF-TOKEN」をコピーする。クッキーは暗号化されているので、認証に使用するには平文にする必要がある",
+ *  operationId="authCookie",
+ *  tags={"auth"},
+ *  @OA\Response(
+ *      response=400,
+ *      description="クエリパラメータに誤りがある",
+ *  ),
+ * @OA\Response(
+ *      response=403,
+ *      description="アクセスが拒否されている",
+ *  ),
+ * @OA\Response(
+ *      response=422,
+ *      description="セマンティックエラーにより、処理を実行できなかった",
+ *  ),
+ * @OA\Response(
+ *      response=500,
+ *      description="不正なエラー",
+ *  ),
+ *  @OA\Response(
+ *      response=200,
+ *      description="Success",
+ *      @OA\Header(
+ *          header="X-XSRF-TOKEN",
+ *          description="The number of remaining requests in the period",
+ *          @OA\Schema( type="string" )
+ *      ),
+ *      @OA\MediaType(mediaType="application/json")
+ *  ),
+ * )
+ */
