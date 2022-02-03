8e5d0d9b45f751d5fef3b02a321a28cce39146d0
murata0531 <coless115@gmail.com>
2022-02-04 02:44:02 +0900

    各種APIのリクエストヘッダに持たせるトークンの定義

--- backend/app/swagger.php
+++ backend/app/swagger.php
@@ -5,5 +5,10 @@
  *     version="8.1.0",
  *     title="GawdiBoard API設計書",
  *     description="APIドキュメントとして参照　テスト実行ができます。",
+ * ),
+ * @OAS\SecurityScheme(
+ *      securityScheme="bearer_token",
+ *      type="http",
+ *      scheme="bearer"
  * )
  */
